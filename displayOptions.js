821b126e2175013a2969a3110249e9b0c4273558
burritojustice <hubgit@oram.com>
2019-08-24 15:51:33 -0700

    fixing bad scene objects

--- displayOptions.js
+++ displayOptions.js
@@ -217,19 +217,19 @@ export const displayOptions = {
     apply: (scene, value, { hexbinInfo, spaceInfo }) => {
       if (value === 0) {
         scene.sources._xyzspace.url = `https://xyz.api.here.com/hub/spaces/${spaceInfo.spaceId}/tile/web/{z}_{x}_{y}`;
-        scene.sources._xyzspace.sources._xyzspace.url_params.tags = '';
+        scene.sources._xyzspace.url_params.tags = '';
       }
       else if (value === 1) {
         console.log('hexbins via',scene.hexbin);
         scene.sources._xyzspace.url = `https://xyz.api.here.com/hub/spaces/${hexbinInfo.spaceId}/tile/web/{z}_{x}_{y}`;
         // will need to grab current zoom and generate appropriate hexbin tag
-        scene.sources._xyzspace.sources._xyzspace.url_params.tags = 'zoom13_hexbin';
+        scene.sources._xyzspace.url_params.tags = 'zoom13_hexbin';
       }
       else if (value === 2) {
         console.log('centroids via',scene.hexbin);
         scene.sources._xyzspace.url = `https://xyz.api.here.com/hub/spaces/${hexbinInfo.spaceId}/tile/web/{z}_{x}_{y}`;
         // will need to grab current zoom and generate appropriate centroid tag
-        scene.sources._xyzspace.sources._xyzspace.url_params.tags = 'zoom13_centroid'
+        scene.sources._xyzspace.url_params.tags = 'zoom13_centroid'
       }
     }
   },
