cc2200296d82e3f35473c17293cc84eeee227cff
burritojustice <hubgit@oram.com>
2019-10-15 16:45:19 -0700

    don't need to actively set extrude if a global

--- displayOptions.js
+++ displayOptions.js
@@ -58,7 +58,6 @@ export const displayOptions = {
         var extrudeProperty =
           `function(feature) {
               try {
-                console.log('extrude',feature${featureExtrusionPropStack.map(k => '[\'' + k + '\']').join('')};)
                 return feature${featureExtrusionPropStack.map(k => '[\'' + k + '\']').join('')};
               }
               catch(e) { return null; } // catches cases where some features lack nested property, or other errors
@@ -66,8 +65,8 @@ export const displayOptions = {
         console.log('extrudeProperty:',extrudeProperty,featureExtrusionPropStack);
         scene.global.lookupFeatureExtrusionProp = extrudeProperty;
         scene.layers._xyz_polygons.draw._polygons_inlay.order = 2001 // move polygons above roads
-        scene.layers._xyz_polygons.draw._polygons_inlay.extrude = scene.global.lookupFeatureExtrusionProp;
-        scene.layers._xyz_polygons._outlines.draw._lines.extrude = scene.global.lookupFeatureExtrusionProp;
+//         scene.layers._xyz_polygons.draw._polygons_inlay.extrude = scene.global.lookupFeatureExtrusionProp;
+//         scene.layers._xyz_polygons._outlines.draw._lines.extrude = scene.global.lookupFeatureExtrusionProp;
         scene.cameras.camera1.type = 'perspective';
       }
       else {
