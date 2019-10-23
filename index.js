b42be009984447afa54674f04bdf9b99130309c9
burritojustice <hubgit@oram.com>
2019-10-22 17:10:44 -0700

    basemap

--- index.js
+++ index.js
@@ -205,12 +205,12 @@ function makeLayer(scene_obj) {
   window.scene = layer.scene;  // debugging
 }
 
-function applySpace({ spaceId, token, hexbinInfo, basemaps, displayToggles: { hexbins } = {} }) {
+function applySpace({ spaceId, token, hexbinInfo, displayToggles: { hexbins } = {} }) {
   if (spaceId && token) {
     // choose main space, or hexbins space
     const activeSpaceId = (hexbins > 0 && hexbinInfo.spaceId != null) ? hexbinInfo.spaceId : spaceId;
     var max_zoom = 18
-    console.log('basemap',basemaps)
+    console.log('basemap',basemap)
     if (basemaps == 'albers') {
         max_zoom = 2 // because tiles
     }
