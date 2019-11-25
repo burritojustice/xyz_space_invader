9c1d6623fa00ea183c6010c1ac4c2b133d7ff269
burritojustice <hubgit@oram.com>
2019-11-24 16:57:12 -0800

    h3 centroids

--- displayOptions.js
+++ displayOptions.js
@@ -208,7 +208,7 @@ export const displayOptions = {
   // toggle XYZ H3 hexbin clustering
   clustering: {
     parse: parseInt,
-    values: [0, 1], // 0 = raw data, 1 = h3 clustering
+    values: [0, 1, 2], // 0 = raw data, 1 = h3 clustering, 2 - h3 hexbin centroids
     // we're using displayOptions for storing and parsing values, but they get applied when creating
     // the Tangram data source in index.js, so there's no `apply()` function here
   },
