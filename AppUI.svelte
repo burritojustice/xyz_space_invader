f4a62dadad5e38c221e016d4d0097842fdfaa633
burritojustice <hubgit@oram.com>
2019-11-26 15:23:50 -0800

    spaceInfo.updatedAt

--- AppUI.svelte
+++ AppUI.svelte
@@ -5,7 +5,8 @@
     <div id="space_info">
       {#if spaceInfo}
         {spaceInfo.title}<br>
-        {spaceId}: {spaceInfo.numFeatures.toLocaleString()} features, {spaceInfo.dataSize}, {spaceInfo.featureSize}/feature<br>
+        {spaceId}: {spaceInfo.numFeatures.toLocaleString()} features, {spaceInfo.dataSize}, {spaceInfo.featureSize}/feature<br
+        {spaceInfo.updatedAt}<br>
       <x style="font-size:10px;">{spaceInfo.description}</x><br>
       {:elseif !spaceLoading}
         <input type="text" placeholder="enter an XYZ space ID" bind:value='spaceId'>
