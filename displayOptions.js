81b9fea67904cd8ff145909ba4a49b23faf4c214
burritojustice <hubgit@oram.com>
2019-12-02 22:40:33 -0800

    stick width it

--- displayOptions.js
+++ displayOptions.js
@@ -202,22 +202,22 @@ export const displayOptions = {
         _.set(scene, 'layers._xyz_dots.draw.points.outline.color', [0,0,0,0.75]);
       }
       else if (value === 4) { // donut outlines
-        _.set(scene, 'layers._xyz_polygons._outlines.draw.overlay_lines.width', '0px');
-        _.set(scene, 'layers._xyz_lines.draw.overlay_lines.outline.width', '0px');
+        _.set(scene, 'layers._xyz_polygons._outlines.draw.overlay_lines.width', '2px');
+        _.set(scene, 'layers._xyz_lines.draw.overlay_lines.outline.width', '2px');
         _.set(scene, 'layers._xyz_dots.draw.points.outline.width', '2px');
         _.set(scene, 'layers._xyz_dots.draw.donut_points.outline.width', '2px');
         donutOutline = true;
       }
       else if (value === 5) { // donut outlines
-        _.set(scene, 'layers._xyz_polygons._outlines.draw.overlay_lines.width', '0px');
-        _.set(scene, 'layers._xyz_lines.draw.overlay_lines.outline.width', '0px');
+        _.set(scene, 'layers._xyz_polygons._outlines.draw.overlay_lines.width', '1px');
+        _.set(scene, 'layers._xyz_lines.draw.overlay_lines.outline.width', '1px');
         _.set(scene, 'layers._xyz_dots.draw.points.outline.width', '1px');
         _.set(scene, 'layers._xyz_dots.draw.donut_points.outline.width', '1px');
         donutOutline = true;
       }
        else if (value === 6) { // donut outlines
-        _.set(scene, 'layers._xyz_polygons._outlines.draw.overlay_lines.width', '0px');
-        _.set(scene, 'layers._xyz_lines.draw.overlay_lines.outline.width', '0px');
+        _.set(scene, 'layers._xyz_polygons._outlines.draw.overlay_lines.width', '.5px');
+        _.set(scene, 'layers._xyz_lines.draw.overlay_lines.outline.width', '.5px');
         _.set(scene, 'layers._xyz_dots.draw.points.outline.width', '.5px');
         _.set(scene, 'layers._xyz_dots.draw.donut_points.outline.width', '.5px');
         donutOutline = true;
