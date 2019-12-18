fe02e1696ee5d13f2f1f576ba97ee61e5f9c77bd
burritojustice <hubgit@oram.com>
2019-12-17 17:20:15 -0800

    quick omv test

--- basemaps.js
+++ basemaps.js
@@ -46,7 +46,8 @@ const xyzTangramBaseScene = addBasePath('tangram_xyz_scene.yaml');
 const xyzTilezenSourceOverride = {
   sources: {
     mapzen: {
-      url: 'https://xyz.api.here.com/tiles/osmbase/512/all/{z}/{x}/{y}.mvt',
+//       url: 'https://xyz.api.here.com/tiles/osmbase/512/all/{z}/{x}/{y}.mvt',
+      url: https://xyz.api.here.com/tiles/herebase.02/{z}/{x}/{y}/omv,
       url_params: {
         'access_token': 'global.xyz_access_token'
       }
