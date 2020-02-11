eacdaaaceda9ba187f03f29201431c8b69b75d9d
john <joram@burrito.local>
2020-02-11 14:21:13 -0800

    fixing applySpace params again

--- index.js
+++ index.js
@@ -235,8 +235,8 @@ function makeLayer(scene_obj) {
   window.layer = layer; // debugging
   window.scene = scene;  // debugging
 }
+function applySpace({ spaceId, token, displayToggles: { hexbins, clustering, clusteringProp } = {}, propertySearchQueryParams, hexbinInfo }, scene_config) {
 
-function applySpace({ spaceId, token, hexbinInfo, displayToggles: { hexbins, clustering, clusteringProp } = {} }, scene_config) {
   if (spaceId && token) {
     // choose main space, or hexbins space
     const activeSpaceId = (hexbins > 0 && hexbinInfo.spaceId != null) ? hexbinInfo.spaceId : spaceId;
