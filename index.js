9438365f3c136c082ecba28f096ca9490af0f8a1
burritojustice <hubgit@oram.com>
2020-03-25 00:58:29 -0700

    checking OR logic on reduction-light and land/water color

--- index.js
+++ index.js
@@ -299,12 +299,12 @@ function applySpace({ spaceId, token, displayToggles: { hexbins, clustering, clu
       } catch(e) {
         console.error("Failed to set scene.view.buffer:\n", e)
       }
-      if (projection == 'globe' && basemap == 'xyz-reduction-dark' || 'xyz-reduction-light') {
+      if (projection == 'globe' && (basemap == 'xyz-reduction-dark' || 'xyz-reduction-light')) {
         // change land color to avoid global shader madness, raise lines above hexbins for better visibility
         toggles.water = 1; // will this raise it? or just the display?
         scene_config.layers.boundaries.country.draw.lines.order = 500;
         scene_config.layers.boundaries.region.draw.lines.order = 500;
-        scene_config.layers.earth.draw.polygons.color = scene_config.global.water_color; // doesn't matter if distorted ocean tiles overlap land
+        scene_config.layers.earth.draw.polygons.color = scene_config.global.water_color; // hide extruding ocean tiles by making land the same color, psyche
         scene_config.layers.water['water-boundary-ocean'].draw.lines.order = 500;
 //         scene_config.sources._xyzspace.url_params.clip = false // hexbins split across tiles get different counts
         map.setMinZoom(2) // hexbin data isn't available below 2 and it's pretty small anyway
